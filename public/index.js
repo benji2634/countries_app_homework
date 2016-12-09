58c5eed5eae970ad4208ba105e8a2a22a97888a0
Tom Benham <benji26@hotmail.co.uk>
2016-12-09 16:00:46 +0000

    refactored to get the map api working on click

--- public/index.js
+++ public/index.js
@@ -1,20 +1,24 @@
 var app = function(){
-  var url = 'https://restcountries.eu/rest/v1/all'
+  var url = 'https://restcountries.eu/rest/v1/all';
   makeRequest(url, requestComplete);
+  
   var countries = [];
+
+  var container = document.getElementById('main-map');
+  var center = {lat: 56.49, lng: -4.2};
+  var mainMap = new MapWrapper(container, center, 5);
+  mainMap.addMarker(center);
 
   var selectBox = document.querySelector('select');
   selectBox.onchange = handleSelectChanged;
+  var geoButton = document.querySelector('#geo-button');
+  geoButton.onclick = geoFindMe;
 }
 
 var makeRequest = function(url, callback) {
-  //create a new XMLHttpRequest
   var request = new XMLHttpRequest();
-  // open the request and tell it what method we want to use
   request.open("GET", url);
-  // set the callback we want to run when complete
   request.onload = callback;
-  // send the request
   request.send();
 }
 
@@ -31,7 +35,7 @@ var populateList = function(allCountries) {
   for (i = 0; i < allCountries.length; i++) {
     var option = document.createElement('option');
     var country = allCountries[i];
-    option.innerText = "Country: " + country.name;
+    option.innerText = country.name;
     option.value = i;
     select.appendChild(option);
   }
@@ -44,7 +48,42 @@ var handleSelectChanged = function( event ) {
   pTag.innerText = "Country: " + country.name + "\nCapital city: " + country.capital + "\nPopulation: " + country.population;
   var countrystring = JSON.stringify(country);
   localStorage.selection = countrystring;
+  var container = document.getElementById('main-map');
+  var latitude  = country.latlng[0];
+  var longitude = country.latlng[1];
+
+  container.innerHTML = '<p>Latitude is ' + latitude + '° <br>Longitude is ' + longitude + '°</p>';
+
+  var location = {lat: latitude, lng: longitude};
+  var mainMap = new MapWrapper(container, location, 5);
+  mainMap.addMarker(location);
 }
 
+var geoFindMe = function() {
+  var container = document.getElementById('main-map');
+  if (!navigator.geolocation){
+    output.innerHTML = "<p>Geolocation is not supported by your browser</p>";
+    return;
+  }
+  function success(position) {
+    var latitude  = position.coords.latitude;
+    var longitude = position.coords.longitude;
+
+    container.innerHTML = '<p>Latitude is ' + latitude + '° <br>Longitude is ' + longitude + '°</p>';
+
+    var location = {lat: latitude, lng: longitude};
+    var mainMap = new MapWrapper(container, location, 10);
+    mainMap.addMarker(location);
+  }
+  function error() {
+    container.innerHTML = "Unable to retrieve your location";
+  }
+  container.innerHTML = "<p>Locating…</p>";
+  navigator.geolocation.getCurrentPosition(success, error);
+}
+
+// var mapCountryLocation = function(event) {
+//   console.log(event.target.value);
+// }
 
 window.onload = app;
